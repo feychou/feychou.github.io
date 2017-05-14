644fffabf7b2da34f34a400ccef6b83a2e8198fe
feychou <federica.recanatini@gmail.com>
2017-05-14 18:29:48 +0200

    Add basic info

--- src/App.js
+++ src/App.js
@@ -25,13 +25,123 @@ class App extends Component {
 
         <div className="App-body">
           <div className="body-column-left">
-            BU
-            <span></span>
+
+            <div className="stats-box">
+              <div className="stats-box-header">
+                <span>Personal Data</span>
+              </div>
+              <div className="stats-box-entries">
+                <div className="stats-box-entry">
+                  <span className="stats-box-tag">Name/Primary Alias</span>
+                  <span>Fey</span>
+                </div>
+
+                <div className="stats-box-entry">
+                  <span className="stats-box-tag">Metatype</span>
+                  <span>Weeaboo Princess</span>
+                </div>
+
+                <div className="stats-box-entry">
+                  <span className="stats-box-tag">Primary Lifestyle</span>
+                  <span>Software Developer</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="stats-box">
+              <div className="stats-box-header">
+                <span>IDS / CONTACTS</span>
+              </div>
+              <div className="stats-box-entries">
+                <div className="stats-box-entry">
+                  <span className="stats-box-tag">Github</span>
+                  <span>
+                    <a target="_blank" href="https://github.com/feychou">feychou</a>
+                  </span>
+                </div>
+
+                <div className="stats-box-entry">
+                  <span className="stats-box-tag">Stackoverflow</span>
+                  <span>
+                    <a target="_blank" href="http://stackoverflow.com/users/story/2255082">feychou</a>
+                  </span>
+                </div>
+
+                <div className="stats-box-entry">
+                  <span className="stats-box-tag">Steam</span>
+                  <span>
+                    <a target="_blank" href="http://steamcommunity.com/id/feychou/">feychou</a>
+                  </span>
+                </div>
+
+                <div className="stats-box-entry">
+                  <span className="stats-box-tag">Facebook</span>
+                  <span>
+                    <a target="_blank" href="https://www.facebook.com/federica.recanatini">federica.recanatini</a>
+                  </span>
+                </div>
+
+                <div className="stats-box-entry">
+                  <span className="stats-box-tag">Kleiderkreisel</span>
+                  <span>
+                    <a target="_blank" href="https://www.kleiderkreisel.at/mitglieder/1518602-ashlotte">ashlotte</a>
+                  </span>
+                </div>
+
+                <div className="stats-box-entry">
+                  <span className="stats-box-tag">email</span>
+                  <span>federica.recanatini@gmail.com</span>
+                </div>
+
+              </div>
+            </div>
           </div>
 
           <div className="body-column-right">
-            BU
-            <span></span>
+
+            <div className="stats-box">
+              <div className="stats-box-header">
+                <span>Skills</span>
+              </div>
+              <div className="stats-box-entries">
+                <div className="stats-box-entry">
+                  <span>Javascript  ES6  ReactJS  Redux</span>
+                </div>
+
+                <div className="stats-box-entry">
+                  <span>Jest</span>
+                </div>
+
+                <div className="stats-box-entry">
+                  <span>Node  Express  Webpack</span>
+                </div>
+
+                <div className="stats-box-entry">
+                  <span>HTML  CSS  XSLT  JSON</span>
+                </div>
+
+                <div className="stats-box-entry">
+                  <span>AWS  S3  Heroku</span>
+                </div>
+
+                <div className="stats-box-entry">
+                  <span>MySQL  DynamoDB </span>
+                </div>
+
+                <div className="stats-box-entry">
+                  <span>Linux  MacOS</span>
+                </div>
+
+                <div className="stats-box-entry">
+                  <span>Git  Github  Jenkins  TravisCI</span>
+                </div>
+
+                <div className="stats-box-entry">
+                  <span>Python  Clojure  Haskell</span>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
